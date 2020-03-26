fb69c3627be153e66098dd0ab6bdf069f6073d35
Aaron Cronin <ac6296@nyu.edu>
2020-03-26 13:32:05 -0400

    data loaded

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 // app.js
+require('./db.js')
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const mongoose = require('mongoose');
+const Review = mongoose.model('Review')
+
+const PORT = 3000;
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    next();
+})
+
+app.get('/', (req, res) => {
+     Review.find({}, (err, reviews) => {
+        res.render('review', {reviews: reviews})
+    })
+
+});
+
+
+
+
+app.listen(PORT)
---
+++ src/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+
+// my schema goes here!
+const Review = new mongoose.Schema({
+    courseNumber: String,
+    courseName: String,
+    semester: String,
+    year: Number,
+    professor: String,
+    review: String
+});
+
+
+mongoose.model('Review', Review);
+mongoose.connect('mongodb://localhost/hw06', {useNewUrlParser: true});
+
+
