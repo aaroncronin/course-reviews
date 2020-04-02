716be2528736abfb776c4c2ff250e343f40006bf
Aaron Cronin <ac6296@nyu.edu>
2020-04-02 18:24:41 -0400

    my reviews updated

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 // app.js
-require('./db.js')
+require('./db.js');
 const express = require('express');
-const session = require('express-session')
+const session = require('express-session');
 const path = require('path');
 const app = express();
 
 const mongoose = require('mongoose');
-const Review = mongoose.model('Review')
+const Review = mongoose.model('Review');
 
 const PORT = 3000;
 
@@ -24,15 +24,26 @@ app.use(session(sessionOptions));
 
 app.use(function(req, res, next) {
     if (req.session.visits === undefined) {
-        req.session.visits = 0
+        req.session.visits = 0;
     }
     req.session.visits += 1;
     res.locals.pageVisits = req.session.visits;
+
+    if (req.session.Reviews === undefined) {
+        req.session.Reviews = [];
+    }
+    if (Object.keys(req.body).length !== 0) {
+        req.session.Reviews.push(req.body);
+    }
+    res.locals.reviews = req.session.Reviews;
+    const headers = req.headers.cookie.split('; ');
+    console.log("\nThe cookie header contains:");
+    headers.forEach((cook) => console.log(cook));
     next();
-})
+});
 
 app.get('/', (req, res) => {
-    let query = {}
+    const query = {};
     if (req.query.semester !== "All" && req.query.semester !== undefined) {
         query['semester'] = req.query.semester;
     }
@@ -40,37 +51,32 @@ app.get('/', (req, res) => {
         query['year'] = req.query.year;
     }
     if (req.query.professor !== "" && req.query.professor !== undefined) {
-        let prof = req.query.professor
-        prof = prof[0].toUpperCase() + prof.slice(1)
+        let prof = req.query.professor;
+        prof = prof[0].toUpperCase() + prof.slice(1);
         query['professor'] = prof;
     }
     Review.find(query, (err, reviews) => {
         res.render('review', {reviews: reviews});
-    })
+    });
 });
 
 app.get('/reviews/add', (req, res) => {
     res.render('add');
-})
+});
 
 app.post('/reviews/add', (req, res) => {
-    // NEED TO DO REQUIREMENTS VERIFICATION
-    let obj = {}
     const a = new Review(req.body);
-    if (typeof(req.body.year) !== Number) {
-        console.log('AHAH')
-    }
-    console.log(a)
     a.save((err, saved) => {
-        if (!err) {
-            res.redirect('/')
+        if (err) {
+            res.render('add', {error: "Review not added. You must enter valid data!"});
+        } else {
+            res.redirect('/'); // doesn't get saved if data input is invalid
         }
-    })
-    
-})
+    });
+});
 
+app.get('/reviews/mine', (req, res) => {
+    res.render('myreviews', {data: res.locals.reviews});
+});
 
-
-
-
-app.listen(PORT)
+app.listen(PORT);
--- src/db.js
+++ src/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-
-
 // my schema goes here!
 const Review = new mongoose.Schema({
     courseNumber: String,
